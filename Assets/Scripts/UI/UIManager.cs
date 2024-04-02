7206f6e687397d8886c879c6920638cba27581d5
Hsalloum <129054795+Hsalloum@users.noreply.github.com>
2024-04-01 22:38:05 -0400

    menu

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,21 +1,38 @@
-
+    
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header ("Game Over")]
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private GameObject winScreen;
     [SerializeField] private AudioClip win;
     [SerializeField] private AudioClip gameover;
 
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
+
 
     private void Awake()
     {
         gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
         winScreen.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseScreen.activeInHierarchy)
+                PauseGame(false);
+            else
+                PauseGame(true);
+        }
+    }
+
+    #region Game Over
     public void GameOver()
     {
         gameOverScreen.SetActive(true);
@@ -34,9 +51,41 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
-    public void Quit()
+
+    public void MainMenu()
     {
-        Application.Quit();
+        SceneManager.LoadScene(0);
     }
 
+    public void Quit()
+    {
+    Application.Quit();
+
+    #if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+
+    }
+    #endregion
+
+    #region Pause
+    public void PauseGame(bool status)
+    {
+        pauseScreen.SetActive(status);
+
+        Time.timeScale = System.Convert.ToInt32(!status);
+   
+
+    }
+
+    public void SoundVolume()
+    {
+        SoundManager.instance.ChangeSoundVolume(0.2f);
+    }
+    public void MusicVolume()
+    {
+        SoundManager.instance.ChangeMusicVolume(0.2f);
+    }
+
+    #endregion
 }
