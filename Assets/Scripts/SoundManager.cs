bd4bb80b278a60e3effca17b66bc3bb1282e6c6d
Hsalloum <129054795+Hsalloum@users.noreply.github.com>
2024-04-01 22:55:37 -0400

    menu

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,15 +5,49 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance { get; private set; }
     private AudioSource source;
+    private AudioSource musicSource;
+
 
     private void Awake()
     {   
         instance = this;
         source = GetComponent<AudioSource>();
+        musicSource = transform.GetChild(0).GetComponent<AudioSource>();
+
+        ChangeSoundVolume(0);
+        ChangeMusicVolume(0);
+
     }
 
     public void PlaySound(AudioClip _sound) { 
         source.PlayOneShot(_sound);
     
+    }
+
+    public void ChangeSoundVolume(float _change)
+    {
+        ChangeSourceVolume(1, "soundVolume", _change, source);
+    }
+
+    private void ChangeSourceVolume(float baseVolume, string volumeName, float change, AudioSource source)
+    {
+        float currentVolume = PlayerPrefs.GetFloat(volumeName, 1);
+                currentVolume += change;
+
+        if (currentVolume > 1)
+            currentVolume = 0;
+        else if (currentVolume < 0)
+            currentVolume = 1;
+
+        float finalVolume = currentVolume * baseVolume;
+        source.volume = finalVolume;
+
+        PlayerPrefs.SetFloat(volumeName, currentVolume);
+
+    }
+
+    public void ChangeMusicVolume(float _change)
+    {
+        ChangeSourceVolume(0.3f, "musicVolume", _change, musicSource);
     }
 }
